74aab16dbdeb3f68fb46e8bf59dd4bf0e4ea30fd
Zahin Nambiar <32119274+Zahin-Nambiar@users.noreply.github.com>
2021-06-09 13:23:29 -0500

    now accepts the AnemometerMsg (#1)
    * now accepts the AnemometerMsg
    
    * resolved most comments, checking wind direction
    
    * edit azimuth in nrg_gas.cpp
    
    * comment edit

--- src/nrg_gas_utilities/include/nrg_gas_utilities/nrg_gas.h
+++ src/nrg_gas_utilities/include/nrg_gas_utilities/nrg_gas.h
@@ -4,8 +4,7 @@
 #include <nrg_gas_utilities/GasSource.h>
 #include <nrg_gas_utilities/WindParameters.h>
 #include <nrg_gas_utilities/GasConcentration.h>
-#include <geometry_msgs/Vector3Stamped.h>
-#include <geometry_msgs/Vector3.h>
+#include <nrg_gas_utilities/AnemometerMsg.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <geometry_msgs/PointStamped.h>
 #include <geometry_msgs/Point.h>
@@ -22,8 +21,8 @@ namespace nrg_gas
 using nrg_gas_utilities::GasSource;
 using nrg_gas_utilities::WindParameters;
 using nrg_gas_utilities::GasConcentration;
-using geometry_msgs::Vector3Stamped;
-using geometry_msgs::Vector3;
+using nrg_gas_utilities::AnemometerMsg;
+
 using geometry_msgs::TransformStamped;
 using geometry_msgs::PointStamped;
 using geometry_msgs::Point;
@@ -34,7 +33,7 @@ class NRGGas
 protected: 
     NRGGas();
 
-    double calculateConcentration(const GasSource &gs, const Vector3Stamped &wind, const TransformStamped map_to_anemometer) const;
+    double calculateConcentration(const GasSource &gs, const AnemometerMsg &wind, const TransformStamped map_to_anemometer) const;
 
     ros::NodeHandle private_nh_;
 
@@ -43,10 +42,10 @@ protected:
 
     WindParameters wp_;   
 
-    std::deque<Vector3Stamped> wind_measurements_;
+    std::deque<AnemometerMsg> wind_measurements_;
     std::deque<GasConcentration> gas_measurements_;
 private:
-    Point calculateSourceTransform(const PointStamped& source, const Vector3Stamped& wind, const TransformStamped& map_to_anemometer) const;
+    Point calculateSourceTransform(const PointStamped& source, const AnemometerMsg& wind, const TransformStamped& map_to_anemometer) const;
 };
     
 } // namespace nrg_gas
